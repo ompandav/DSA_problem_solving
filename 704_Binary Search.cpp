28f82fadb0402ab27d39ba7cfb0c4e43f93347be
Om Pandav <124763727+ompandav@users.noreply.github.com>
2023-02-10 11:55:51 +0530

    Create 704_Binary Search.cpp

---
+++ 704_Binary Search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s=0, e=nums.size()-1, mid;
+        while(s<=e){
+            mid = s+(e-s)/2;
+
+            if(target == nums[mid]){
+                return mid;
+            }
+            if(target < nums[mid]){
+                e = mid-1;
+            }
+            if(target > nums[mid]){
+                s = mid+1;
+            }
+        }
+        return -1;
+    }
+};
