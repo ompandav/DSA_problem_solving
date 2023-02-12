cf06522eaaa96a7d16fa77551037ec2fbc682592
Om Pandav <124763727+ompandav@users.noreply.github.com>
2023-02-12 12:31:28 +0530

    Create 1_Two Sum.cpp

---
+++ 1_Two Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        vector<int> ans(2);
+        for(int i=0; i<arr.size(); i++) {
+            for(int j=i+1; j<arr.size(); j++){
+                if(arr[i]+arr[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+                }
+            }
+        }
+        return ans;
+    }
+};
