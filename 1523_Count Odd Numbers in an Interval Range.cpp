b3d7a78a852680667d93597efc08fd40d7eed309
Om Pandav <124763727+ompandav@users.noreply.github.com>
2023-02-14 00:19:12 +0530

    Create 1523_Count Odd Numbers in an Interval Range.cpp

---
+++ 1523_Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int ans = 0;
+        if(low%2 == 0 & high%2 == 0){
+            ans = ( high - low )/2;
+        }
+        else{
+            ans = ( high - low )/2 + 1;
+        }
+
+        return ans;
+    }
+};
