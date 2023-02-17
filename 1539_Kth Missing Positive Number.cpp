a8fc27b9c9a7c721098d7000950db104ce0449ea
Om Pandav <124763727+ompandav@users.noreply.github.com>
2023-02-17 23:50:00 +0530

    Create 1539_Kth Missing Positive Number.cpp

---
+++ 1539_Kth Missing Positive Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int lo = 0, hi = arr.size(), mid;
+        while(lo < hi) {
+            mid = (lo + hi) / 2;
+            if(arr[mid] - mid - 1 < k) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo + k;
+    }
+};
